5b391149bee2a99fdde7cc397ba356e43fdf70d6
simislearning <64547883+simislearning@users.noreply.github.com>
2023-12-29 14:22:48 -0800

    Delete init/Enable View in File Explorer.ps1

--- init/Enable View in File Explorer.ps1
+++
@@ -1,8 +0,0 @@
-#Connect to SharePoint Online Management Shell by running:
-#requeires SPOService Module 
-#https://learn.microsoft.com/en-us/powershell/sharepoint/sharepoint-online/connect-sharepoint-online?view=sharepoint-ps
-#log into tenant as Global Admin using powershell
-Connect-SPOService -Url https://contoso-admin.sharepoint.com
-
-# Enable View in File Explorer for Edge
-Set-SPOTenant -ViewInFileExplorerEnabled $True
